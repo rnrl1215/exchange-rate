504efd5de1e7614e2727c3aeb69cc084375688b9
rnrl1215 <d1215@naver.com>
2021-12-09 16:31:42 +0900

    added exception handler exception

--- src/main/java/toyproject/exchangerate/exception/ExceptionHandler.java
+++ src/main/java/toyproject/exchangerate/exception/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package toyproject.exchangerate.exception;
 
 import com.sun.jdi.InternalException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,16 +9,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
+@Slf4j
 @ControllerAdvice
 public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     public String exchangeRateUserException(Exception e, Model model) {
+        log.warn("Exception: "+e.getMessage());
         model.addAttribute("error",e.getMessage());
         return "/exchangeRateForm :: #resultDiv";
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(InternalException.class)
     public String exchangeRateInternalException(Exception e, Model model) {
+        log.warn("Exception: "+e.getMessage());
         model.addAttribute("error",e.getMessage());
         return "/exchangeRateForm :: #resultDiv";
     }
